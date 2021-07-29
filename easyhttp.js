7516ce3021020703c83177ea81eb3d7b7a72f91f
juloelker <35081946+juloelker@users.noreply.github.com>
2021-07-29 12:41:53 -0400

    Add files via upload

--- easyhttp.js
+++ easyhttp.js
@@ -2,30 +2,41 @@ function easyHTTP() {
   this.http = new XMLHttpRequest();
 }
 
-//Make an HTTP GET request
-easyHTTP.prototype.get = function (url) {
+//HTTP GET request
+easyHTTP.prototype.get = function (url, callback) {
   this.http.open("GET", url, true);
 
-  //the second this (line 15) is function
-  //scope, not the parent prototype scope, will not work
-  //can be corrected by the ES6 arrow function or the
-  //lexical this (shown below)
-
-  // this.http.onload = function () {
-  //   if (this.http.status === 200) {
-  //     console.log(this.http.responseText);
-  //   }
-
-  // In ES5, correct the onload function by setting
-  //a let variable to capture the this prototype
-
   let self = this;
-
   this.http.onload = function () {
     if (self.http.status === 200) {
-      console.log(self.http.responseText);
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
     }
   };
-
   this.http.send();
 };
+
+//HTTP POST request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
+
+//HTTP PUT
+easyHTTP.prototype.put = function (url, data1, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  let self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data1));
+};
+
+//HTTP DELETE
